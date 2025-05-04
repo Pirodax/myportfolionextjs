4c8fbfdc39de9e157519fb1b16a4f8af7d16dde7
Pirodax <ludo.bergeron@icloud.com>
2025-05-04 11:57:14 +0200

    Probleme résolut avec succès hehe

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -25,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -33,6 +35,7 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="dark"
             enableSystem
+            enableColorScheme       
             disableTransitionOnChange
           >
             {children}
