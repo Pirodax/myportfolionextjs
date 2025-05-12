7652da3ef29f95823eef8e6405297cf81493b9b7
Pirodax <ludo.bergeron@icloud.com>
2025-05-12 17:56:55 +0200

    my aproach fini passe au footer de la page

--- components/TextGenerateEffectDemo.tsx
+++ components/TextGenerateEffectDemo.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
-const words = `Yo c'est ludovic le meilleur dev du monde
-`;
+const words = `Bonjour moi c'est Ludovic et voici mon Portfolio`;
 
 export function TextGenerateEffectDemo() {
   return <TextGenerateEffect className = "texte-center text-[40px] md:text-5xl lg:text-6xl justify-center" 
