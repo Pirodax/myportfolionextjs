bbedeee9e4ea6acd8642047eed89f45be38146ca
Pirodax <ludo.bergeron@icloud.com>
2025-05-22 03:16:00 +0200

    modification des mots des cola (insitation à laissé un commentaire )

--- data/index.ts
+++ data/index.ts
@@ -123,31 +123,31 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Ludovicccc vraiment trop cool.",
+      "Soyez un des premiers à laisser un commentaire! Envoyez-moi un message privé et j'ajouterai votre contribution par la suite 😉",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "ludo ludo ludo.",
+      "Soyez un des premiers à laisser un commentaire! Envoyez-moi un message privé et j'ajouterai votre contribution par la suite 😉",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "nonononon",
+      "Soyez un des premiers à laisser un commentaire! Envoyez-moi un message privé et j'ajouterai votre contribution par la suite 😉",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Soyez un des premiers à laisser un commentaire! Envoyez-moi un message privé et j'ajouterai votre contribution par la suite 😉",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Soyez un des premiers à laisser un commentaire! Envoyez-moi un message privé et j'ajouterai votre contribution par la suite 😉",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
