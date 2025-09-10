35602fd426dbe0ba12cb16904c753a5a06c20746
Pirodax <ludo.bergeron@icloud.com>
2025-09-10 17:52:47 +0200

    add speed-insights to the projet

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/ui/ThemeProvider"
 import  ContactButtonFix   from '@/components/ContactButtonFix'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -43,6 +44,7 @@ export default function RootLayout({
             {children}
             <ContactButtonFix/>
             <Analytics />
+            <SpeedInsights/>
           </ThemeProvider>
 
         </body>
